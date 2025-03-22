28f6d6a4ea9516da8f6f35e1e0b4441353d181a6
nane984 <br.scekic@gmail.com>
2025-03-22 15:45:28 -0400

    add model carMake and carModel. Fill data

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='register', view=views.registration, name='register'),
 
+    # path for get cars reviews view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    
     # path for dealer reviews view
 
     # path for add a review view
