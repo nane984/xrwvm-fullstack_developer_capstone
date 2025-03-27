a2f7922ff7f618735622a50b8a566a379503442f
Branko Scekic <br.scekic@gmail.com>
2025-03-27 13:54:50 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -16,7 +16,7 @@ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params= params+ key+ "="+ value+ "&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -28,7 +28,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except Exception as e:
         # If any error occurs
-        print("Network exception occurred")
+        print("Network exception occurred {e}")
 
 
 def analyze_review_sentiments(text):
@@ -49,5 +49,5 @@ def post_review(data_dict):
         print(response.json())
         return response.json()
     except Exception as e:
-        print("Network exception occurred")
+        print("Network exception occurred {e}")
     # Add code for posting review
