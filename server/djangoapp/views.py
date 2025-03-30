681770f9b7210139fe0f7e9fe99a95a13b851426
nane984 <br.scekic@gmail.com>
2025-03-30 10:49:06 -0400

    rewiev

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -141,10 +141,10 @@ def get_dealer_details(request, dealer_id):
 def add_review(request):
     # if (request.user.is_anonymous == False):
     if (request.user.is_anonymous_is_(False)):
-        # data = json.loads(request.body)
+        data = json.loads(request.body)
         try:
-            # response = post_review(data)
-            return JsonResponse({"status": 200})
+            response = post_review(data)
+            return JsonResponse({"status": 200}, "review":response)
         except Exception as e:
             # If any error occurs
             print(f"Unexpected {e=}, {type(e)=}")
